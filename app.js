0bcd4d2f5b8a5b0520aa04a2eb313414a142f7d3
Greg App <greg358@mail.ru>
2022-08-23 19:14:11 +0300

    feat: add: choose AncCardset create/display curCardSet

--- app.js
+++ app.js
@@ -5,17 +5,68 @@ console.log(ancientsData);
 
 let a = ancientsData.findIndex((el) => {return el.name==="azathoth";});
 console.log(a); */
-
-/*---Create ancients Pics list----*/
-function createAncPicList() {
 const ancDataList = ancientsData.slice();
 const ancPicList = document.querySelector('.ancients-container');
-
+ancPicList.addEventListener('click',createAncCardSet);
+/*---Create ancients Pics list----*/
+function createAncPicList() { 
 ancDataList.forEach((el)=>{
     const ancPic= document.createElement('div');
 ancPicList.append(ancPic);
 ancPic.classList.add('ancient-card');
 ancPic.setAttribute('style',`background-image: url('${el.cardFace}')`);
+ancPic.setAttribute('id',`${el.id}`);
 });
 }
 createAncPicList();
+
+/*---Create card set data----*/
+let curCardSet = {};
+function createAncCardSet(e) {
+   if(e.target.classList.contains('ancient-card')) { const ancActiveList = document.querySelectorAll('.option-active');
+       
+     if(ancActiveList) {
+        ancActiveList.forEach((el)=>{
+            if (el.classList.contains('option-active')&&(el.id!==e.target.id)) {
+            el.classList.remove('option-active');}
+        });
+        }
+ if (!e.target.classList.contains('option-active')) {
+    let ind = ancientsData.findIndex(el=> el.id===e.target.id);
+    Object.assign(curCardSet,ancientsData[ind]);
+    e.target.classList.add('option-active');
+    showCurCardSet(); 
+ } 
+   } 
+}
+
+/*-----show Card Set----------*/
+function showCurCardSet() {
+    if (curCardSet) {
+       const ancSetTable = document.querySelectorAll('.dot');
+       let stages =[];
+       let cardSetKeys =Object.keys(curCardSet);
+       cardSetKeys.forEach ((el)=>{
+        if (el.includes('first')) {
+            stages.push(curCardSet[el].greenCards,curCardSet[el].brownCards,curCardSet[el].blueCards);
+           
+           }
+           if (el.includes('second')) {
+            stages.push(curCardSet[el].greenCards,curCardSet[el].brownCards,curCardSet[el].blueCards);
+            
+           }
+           if (el.includes('third')) {
+            stages.push(curCardSet[el].greenCards,curCardSet[el].brownCards,curCardSet[el].blueCards);
+            
+            for (let i=0; i<ancSetTable.length;i++) {
+                ancSetTable[i].textContent =stages[i];
+            }
+           }
+       }); 
+       } 
+       console.log(curCardSet);
+       
+    
+    
+}
+ 
